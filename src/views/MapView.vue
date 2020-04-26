e5040eecf76185da01168ee10bf7425b5e1d87cd
Achim Rohn <achim@rohn.eu>
2020-04-26 23:20:56 +0200

    Add better styling padding

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="mapView">
-    <h2>Take a look at how many people joined the hackathon</h2>
-    <h3>Our team wanted to say thank you with <span class="heart">❤</span>️
-      to all supporters, creators and attendees of this hackathon.</h3>
-    <h3>To be part a part of it, go to the sign up <button @click="signUp">Sign Up</button></h3>
+    <div class="introduction">
+      <h2>Take a look at how many people joined the hackathon</h2>
+      <h3>Our team wanted to say thank you with <span class="heart">❤</span>️
+        to all supporters, creators and attendees of this hackathon.</h3>
+      <h3>To be a part of it, go to the sign up
+        <button @click="signUp">Sign Up</button>
+      </h3>
+    </div>
     <div class="map">
       <LeafletMap/>
       <h3 class="thankYou">Say thank you to us and pay us with a "like" here:
-        <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank" rel="noopener noreferrer">Link</a></h3>
-      </div>
-      <footer>
-        <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank"><img src="@/assets/Banner.jpg" alt="" srcset=""></a>
-      </footer>
+        <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank"
+           rel="noopener noreferrer">Link</a></h3>
+    </div>
+    <footer>
+      <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank"><img src="@/assets/Banner.jpg"
+                                                                                            alt="" srcset=""></a>
+    </footer>
   </div>
 </template>
 
@@ -33,41 +39,48 @@ export default class MapView extends Vue {
 </script>
 
 <style lang="scss">
-.mapView{
-  margin: 2rem auto;
-  /*min-height: 900px;*/
-  max-width: 900px;
-  position:relative;
-  .map{
-    padding: 2rem 4.5rem;
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.05);
-    border-radius: 1rem;
+  .mapView {
+    margin: 2rem auto;
+    /*min-height: 900px;*/
+    max-width: 900px;
+    position: relative;
+
+    .map {
+      padding: 2rem 4.5rem;
+      box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.05);
+      border-radius: 1rem;
+    }
   }
-}
 
-.thankYou {
-  margin-top: 1em;
+  .thankYou {
+    margin-top: 1em;
 
-  a {
+    a {
+      color: red;
+    }
+  }
+
+  .introduction {
+    padding: 1em;
+  }
+
+  .heart {
     color: red;
   }
-}
 
-.heart {
-  color: red;
-}
+  footer {
+    text-align: center;
+    font-weight: bold;
+    /*position: absolute;*/
+    bottom: 0;
+    max-width: 100%;
 
-footer{
-  text-align: center;
-  font-weight: bold;
-  /*position: absolute;*/
-  bottom: 0;
-  max-width: 100%;
-  p{
-    padding: 1rem 0;
+    p {
+      padding: 1rem 0;
+    }
+
+    img {
+      width: 100%;
+    }
   }
-  img{
-    width: 100%;
-  }
-}
 </style>
