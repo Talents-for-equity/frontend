24a7dfd313959096f55191576c4e7078d14e4aaf
thamaragerigr <thamaragerigr@gmail.com>
2020-04-26 21:05:37 +0200

    footer

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+  <div class="mapView">
     <div class="map">
       <LeafletMap/>
       </div>
+      <footer>
+        <p>Our Team wanted to say thank you with :hearts: to all Supporter, Creator and Attendees of this Hackathon.</p>
+        <img src="@/assets/Banner.jpg" alt="" srcset="">
+        <p>Say thank you to us and pay us with a "like" here: <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank" rel="noopener noreferrer">Link</a></p>
+      </footer>
   </div>
 </template>
 
@@ -19,9 +24,25 @@ export default class MapView extends Vue {
 }
 </script>
 
-<style>
-.map{
-  padding: 2rem;
-  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.03);
+<style lang="scss">
+.mapView{
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  .map{
+    padding: 2rem;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.03);
+  }
+}
+
+footer{
+  text-align: center;
+  font-weight: bold;
+  p{
+    padding: 2rem 0;
+  }
+  img{
+    width: 100%;
+  }
 }
 </style>
