2278a5845886721a28cce2a45e8ce6f9ddd694ec
Achim Rohn <achim@rohn.eu>
2020-04-26 23:09:45 +0200

    Add more introduction text to map view

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="mapView">
+    <h2>Take a look at how many people joined the hackathon</h2>
+    <h3>Our team wanted to say thank you with <span class="heart">❤</span>️
+      to all supporters, creators and attendees of this hackathon.</h3>
+    <h3>To be part a part of it, go to the sign up <button @click="signUp">Sign Up</button></h3>
     <div class="map">
       <LeafletMap/>
+      <h3 class="thankYou">Say thank you to us and pay us with a "like" here:
+        <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank" rel="noopener noreferrer">Link</a></h3>
       </div>
       <footer>
-        <p>Our Team wanted to say thank you with :hearts: to all Supporter, Creator and Attendees of this Hackathon.</p>
         <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank"><img src="@/assets/Banner.jpg" alt="" srcset=""></a>
-        <p>Say thank you to us and pay us with a "like" here: <a href="https://devpost.com/software/talents-for-equity-w5hnuc" target="_blank" rel="noopener noreferrer">Link</a></p>
       </footer>
   </div>
 </template>
@@ -14,33 +18,49 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import LeafletMap from '@/components/LeafletMap.vue'
+import router from '@/router/index'
+
 @Component({
   components: {
     LeafletMap
   }
 })
 export default class MapView extends Vue {
-
+  async signUp () {
+    await router.push('/preregistration')
+  }
 }
 </script>
 
 <style lang="scss">
 .mapView{
   margin: 2rem auto;
-  min-height: 1100px;
+  /*min-height: 900px;*/
   max-width: 900px;
   position:relative;
   .map{
-    padding: 7rem 4.5rem;
+    padding: 2rem 4.5rem;
     box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.05);
     border-radius: 1rem;
   }
 }
 
+.thankYou {
+  margin-top: 1em;
+
+  a {
+    color: red;
+  }
+}
+
+.heart {
+  color: red;
+}
+
 footer{
   text-align: center;
   font-weight: bold;
-  position: absolute;
+  /*position: absolute;*/
   bottom: 0;
   max-width: 100%;
   p{
