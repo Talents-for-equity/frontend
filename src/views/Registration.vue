edaf258e578bf17b72dd3037fd1aad280001d26c
Achim Rohn <achim@rohn.eu>
2020-04-27 02:34:11 +0200

    Add autocomplete country dropdown

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -2,8 +2,8 @@
   <div>
     <div id="isSeekerTalent">
       <h2>Are you a</h2>
-      <input v-on:click="userStatus" class="blue-button" type="button" value="Seeker" />
-      <input class="blue-button" type="button" value="Talent" />
+      <input v-on:click="userStatus" class="blue-button" type="button" value="Seeker"/>
+      <input class="blue-button" type="button" value="Talent"/>
     </div>
 
     <RegistrationForm
@@ -11,7 +11,13 @@
       :user-is-seeker="userIsSeeker"
       v-if="!showSuccessful"
     />
-    <RegistrationSuccessful v-if="showSuccessful" />
+    <RegistrationSuccessful v-if="showSuccessful"/>
+    <div>
+      <div class="countryLabel">Country</div>
+      <md-autocomplete class="countryInput"
+                       v-model="selectedCountry" :md-options="countries">
+      </md-autocomplete>
+    </div>
   </div>
 </template>
 
@@ -29,8 +35,12 @@ import IsSeekerTalent from '@/components/IsSeekerTalent.vue'
   }
 })
 export default class Registration extends Vue {
-  private showSuccessful = false;
-  private userIsSeeker = false;
+  private showSuccessful = false
+  private userIsSeeker = false
+
+  selectedCountry = null
+
+  countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua & Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia & Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait', 'Kyrgyz Republic', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre & Miquelon', 'Samoa', 'San Marino', 'Satellite', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts & Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor L\'Este', 'Togo', 'Tonga', 'Trinidad & Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks & Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe']
 
   register () {
     console.log('more Jägermeister')
@@ -45,5 +55,18 @@ export default class Registration extends Vue {
 }
 </script>
 
-<style>
+<style lang="scss">
+  .countryInput {
+    margin: 0;
+  }
+
+  #app button {
+    background: none;
+    width: 32px;
+  }
+
+  .countryLabel {
+    position: relative;
+    top: 14px;
+  }
 </style>
